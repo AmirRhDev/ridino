4e5bc1243a8f6c3123cf3509480f655aa27beb20
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-06-15 15:18:19 +0330

    [ADD] product card & list static template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Hero from "@/components/features/hero";
+import ProductList from "@/components/features/product-list/product-list";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 items-center">
       <Hero />
+
+      <div className="w-full px-11">
+        <ProductList />
+      </div>
     </main>
   );
 }
---
+++ src/components/features/product-list/product-list.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import carImage from "../../../../public/car1.jpg";
+import { MapPin } from "lucide-react";
+import TomanIcon from "@/components/icons/toman";
+import { Badge } from "@/components/shadcnUi/badge";
+
+function ProductList() {
+  return (
+    <ul className="grid grid-cols-4 gap-6">
+      <li className="flex flex-col border border-foreground/30 shadow-muted shadow-sm rounded">
+        <Image
+          className="w-full rounded-t h-44"
+          src={carImage}
+          alt="car image"
+        />
+
+        <div className="py-3 px-2">
+          <div className="flex justify-between items-center gap-1">
+            <p className="text-foreground font-bold text-lg">
+              رنو ساندرو استپ وی
+            </p>
+            <span className="text-foreground/80 text-xs">لحظاتی پیش</span>
+          </div>
+
+          <div className="flex items-center text-foreground/80 text-sm gap-1">
+            <p>1404</p>
+            <span>-</span>
+            <p>صفر کیلومتر</p>
+            <span>-</span>
+            <p>اتومات</p>
+          </div>
+
+          {/* features */}
+          <div className="flex flex-wrap gap-1 py-4">
+            <Badge variant="special">بیمه کامل</Badge>
+            <Badge variant="destructive">فروش فوری</Badge>
+          </div>
+
+          <div className="flex items-center justify-between gap-1">
+            <div className="flex items-center gap-0.5">
+              <MapPin size={18} strokeWidth="1.5" />
+              <span className="text-foreground/90 font-light">تهران</span>
+            </div>
+
+            <p className="flex gap-1 font-semibold text-foreground">
+              <span>980,000,000</span>
+              <TomanIcon />
+            </p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default ProductList;
