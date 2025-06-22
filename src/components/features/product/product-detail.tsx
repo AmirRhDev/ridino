984749a307e5ceff4978f4391c738281e6a3787f
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-06-22 10:53:06 +0330

    [ADD] product detail

---
+++ src/components/features/product/product-detail.tsx
@@ -0,0 +1,13 @@
+function ProductDetail() {
+  return (
+    <div className="flex items-center text-foreground/90 gap-1 mt-2.5">
+      <p>1404</p>
+      <span>-</span>
+      <p>صفر کیلومتر</p>
+      <span>-</span>
+      <p>اتومات</p>
+    </div>
+  );
+}
+
+export default ProductDetail;
