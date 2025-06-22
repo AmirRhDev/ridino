0d8122f5f3380143b304560e801925e15b0fead3
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-06-22 10:52:45 +0330

    [ADD] product description

---
+++ src/components/features/product/product-description.tsx
@@ -0,0 +1,13 @@
+function ProductDescription() {
+  return (
+    <div className="flex flex-col mt-2 pb-3 border-b border-foreground/10">
+      <h3 className="font-semibold text-foreground text-lg">توضیحات</h3>
+      <p className="text-foreground/80">
+        فروش فوری زیر قیمت بازار خشک مدارک آماده انتقال سند آزاد گارانتی فعال
+        تحویل فوری لطفا فقط تماس بگیرید عرشیا نادری
+      </p>
+    </div>
+  );
+}
+
+export default ProductDescription;
