c9753b01ce192953189e7fbc4755a1ece549bff1
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-06-22 10:53:34 +0330

    [ADD] product price

---
+++ src/components/features/product/product-price.tsx
@@ -0,0 +1,13 @@
+import TomanIcon from "@/components/icons/tomanIcon";
+
+function ProductPrice() {
+  return (
+    <p className="flex gap-1 font-semibold text-foreground">
+      <span>980,000,000</span>
+
+      <TomanIcon />
+    </p>
+  );
+}
+
+export default ProductPrice;
