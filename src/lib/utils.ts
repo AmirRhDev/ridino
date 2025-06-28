a6e71a9fa8e61f399be6ad525d428a43b06db1a3
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-06-28 12:20:50 +0330

    [ADD] dayjs and setup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import dayjs from "@/lib/dayjs";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function timeAgo(isoDate: string): string {
+  return dayjs(isoDate).fromNow();
+}
+
+export function formatPrice(
+  price: number,
+  lang: "fa-IR" | "en-US" = "fa-IR",
+): string {
+  return price.toLocaleString(lang);
 }
