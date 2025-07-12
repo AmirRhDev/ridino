8a13ae65e534dd85b92b7e0cb1171de864a7e4ef
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-07-12 13:36:47 +0330

    [ADD] signIn, signOut, current user utils [EDIT] sign up util

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,27 @@
 import { supabase } from "@/lib/supabaseClient";
 
 export async function supabaseSignUp(email: string, password: string) {
-  const { error } = await supabase.auth.signUp({ email, password });
+  const { data, error } = await supabase.auth.signUp({ email, password });
 
   if (error) throw error;
+
+  return data;
+}
+
+export async function supabaseSignIn(email: string, password: string) {
+  const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+  if (error) throw error;
+}
+
+export async function signOutUser() {
+  const { error } = await supabase.auth.signOut({ scope: "local" });
+
+  if (error) throw error;
+}
+
+export async function getCurrentUser() {
+  const { data } = await supabase.auth.getUser();
+
+  return data.user;
 }
