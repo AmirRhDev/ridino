b657d1e81a86b57fa55105598ab61501149d1d64
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-07-12 13:35:58 +0330

    [ADD] sign in route

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import SignIn from "@/components/features/auth/sign-in";
+
+function Page() {
+  return (
+    <div className="max-w-3/5 m-auto">
+      <SignIn />
+    </div>
+  );
+}
+
+export default Page;
