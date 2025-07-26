554c74dc9f1fd4734e647a5ec68ac5df4afb0d00
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-07-26 15:23:33 +0330

    [ADD] car_iamges table

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,12 +1,34 @@
 import { supabase } from "@/lib/supabaseClient";
 
-export async function uploadImages(
-  carId: string,
-  files: readonly File[],
-): Promise<void> {
+export const uploadImages = async (carId: string, files: readonly File[]) => {
+  const uploadedUrls: string[] = [];
+
   for (const file of files) {
-    await supabase.storage
+    const filePath = `${carId}/${crypto.randomUUID()}-${file.name}`;
+
+    const { data, error } = await supabase.storage
       .from("cars-images")
-      .upload(`${carId}/${crypto.randomUUID()}-${file.name}`, file);
+      .upload(filePath, file);
+
+    if (error) throw error;
+
+    const publicUrl = supabase.storage
+      .from("cars-images")
+      .getPublicUrl(filePath).data.publicUrl;
+
+    uploadedUrls.push(publicUrl);
+
+    const { error: insertError } = await supabase.from("car_images").insert({
+      car_id: carId,
+      url: publicUrl,
+    });
+
+    if (insertError) {
+      console.log("insertError", insertError);
+      console.error("Failed to save image metadata:", insertError.message);
+      // TODO: handle error
+    }
   }
-}
+
+  return uploadedUrls;
+};
