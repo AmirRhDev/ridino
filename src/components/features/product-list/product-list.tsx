a41e95ae706b960e65b2ed2baa61c4ff7281a9eb
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-07-27 13:42:44 +0330

    [EDIT] className prop for car list

--- src/components/features/product-list/product-list.tsx
+++ src/components/features/product-list/product-list.tsx
@@ -1,13 +1,20 @@
 import { CarType } from "@/types/product";
 import ProductCard from "./product-card";
+import { cn } from "@/lib/utils";
 
 interface ProductListProps {
   items?: CarType[];
+  className?: string;
 }
 
-function ProductList({ items }: ProductListProps) {
+function ProductList({ items, className }: ProductListProps) {
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div
+      className={cn(
+        "grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6",
+        className,
+      )}
+    >
       {items?.map((item) => <ProductCard key={item.id} {...item} />)}
     </div>
   );
