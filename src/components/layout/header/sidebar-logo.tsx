415a1488fa0a768f3424245766969abc39131ceb
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-09-11 13:01:27 +0330

    [ADD] logo

--- src/components/layout/header/sidebar-logo.tsx
+++ src/components/layout/header/sidebar-logo.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
-import logo from "../../../../public/logo.png";
+import logo from "../../../../public/logo.svg";
 import Link from "next/link";
 
 function SidebarLogo() {
   return (
     <div className="overflow-hidden">
       <Link href="/">
-        <Image className="scale-200" height={35} src={logo} alt="ridino logo" />
+        <Image width={90} src={logo} alt="ridino logo" />
       </Link>
     </div>
   );
