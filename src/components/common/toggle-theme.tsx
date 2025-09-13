e606d42c741effda57a747955865b8541344c282
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-09-13 10:25:35 +0330

    [EDIT] fix bug in toggle theme component

--- src/components/common/toggle-theme.tsx
+++ src/components/common/toggle-theme.tsx
@@ -3,18 +3,24 @@ import { useTheme } from "next-themes";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { Button } from "@/components/shadcnUi/button";
 
-export default function ToggleTheme() {
+interface Props {
+  withLabel?: boolean;
+}
+
+export default function ToggleTheme({ withLabel }: Props) {
   const { theme, setTheme } = useTheme();
+
+  const isDark = theme === "dark";
 
   return (
     <Button
-      size="icon"
+      size={withLabel ? "default" : "icon"}
       variant="outline"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(isDark ? "light" : "dark")}
     >
-      <MoonIcon className="dark:-rotate-90 rotate-0 scale-100 transition-all dark:scale-0" />
-      <SunIcon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
+      {isDark ? <MoonIcon /> : <SunIcon />}
+      {withLabel && (isDark ? "تم تاریک" : "تم روشن")}
+      <span className="sr-only">تغییر تم</span>
     </Button>
   );
 }
