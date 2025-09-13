baff15f1c0d290635763869a0dfbdf2ceef9f836
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-09-13 10:23:28 +0330

    [EDIT] search car page, handle aprams, handle loading state

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,24 @@
 import SearchField from "@/components/common/search-field";
 import SwitchField from "@/components/common/switch-field";
 import { AllProduct } from "@/components/features/product-list-container";
-import CategoryList from "@/components/features/product-list/category-list";
+import ProductLoader from "@/components/features/product-list/product-loader";
 import { ProductsSortFilter } from "@/components/features/product-list/product-sort-filter";
+import { SortFilterType } from "@/types/product";
+import { Suspense } from "react";
 
-function page() {
+interface Props {
+  searchParams: Promise<{ title?: string; sort?: string; fixed?: string }>;
+}
+
+async function page({ searchParams }: Props) {
+  const params = await searchParams;
+  const title = params.title || "";
+  const sort = (params.sort as SortFilterType) || "newest";
+  const hasFixedPrice = params.fixed === "true";
+
   return (
     <>
-      <div className="w-full flex flex-col gap-3 border-b border-foreground/30 pb-4 mb-4">
+      <div className="w-full flex flex-col gap-3 border-b border-border pb-4 mb-4">
         <div className="flex flex-col sm:flex-row sm:flex-wrap md:flex-nowrap items-center gap-3">
           <SearchField />
 
@@ -16,10 +27,20 @@ function page() {
           <SwitchField />
         </div>
 
-        <CategoryList />
+        {/* <CategoryList /> */}
       </div>
 
-      <AllProduct />
+      <Suspense
+        fallback={
+          <ProductLoader className="sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6" />
+        }
+      >
+        <AllProduct
+          searchedTitle={title}
+          sort={sort}
+          hasFixedPrice={hasFixedPrice}
+        />
+      </Suspense>
     </>
   );
 }
