8d6295335b3c1887ea479f88b38c2dad7def0397
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-09-13 10:20:33 +0330

    [EDIT] handle loading state in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import SearchField from "@/components/common/search-field";
 import Hero from "@/components/features/hero";
 import { AllProduct } from "@/components/features/product-list-container";
-import CategoryList from "@/components/features/product-list/category-list";
+import ProductLoader from "@/components/features/product-list/product-loader";
 import { ProductsSortFilter } from "@/components/features/product-list/product-sort-filter";
 import ProductStaticFilter from "@/components/features/product-list/product-static-filter";
 import { SortFilterType } from "@/types/product";
+import { Suspense } from "react";
 
 interface HomeProps {
   searchParams: Promise<{ title?: string; sort?: string; fixed?: string }>;
@@ -20,7 +21,7 @@ export default async function Home({ searchParams }: HomeProps) {
     <>
       <Hero />
 
-      <div className="w-full flex flex-col gap-3 border-b border-foreground/30 pb-4 mb-4">
+      <div className="w-full flex flex-col gap-3 border-b border-border pb-4 mb-4">
         <div className="flex flex-col sm:flex-row sm:flex-wrap md:flex-nowrap items-center gap-3">
           <SearchField defaultValue={title} />
 
@@ -29,14 +30,20 @@ export default async function Home({ searchParams }: HomeProps) {
           <ProductStaticFilter defaultValue={hasFixedPrice} />
         </div>
 
-        <CategoryList />
+        {/* <CategoryList /> */}
       </div>
 
-      <AllProduct
-        searchedTitle={title}
-        sort={sort}
-        hasFixedPrice={hasFixedPrice}
-      />
+      <Suspense
+        fallback={
+          <ProductLoader className="sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6" />
+        }
+      >
+        <AllProduct
+          searchedTitle={title}
+          sort={sort}
+          hasFixedPrice={hasFixedPrice}
+        />
+      </Suspense>
     </>
   );
 }
