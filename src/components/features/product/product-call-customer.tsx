20a5111d7210224e40b3c52b80438ea8e97ad731
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-09-13 10:26:45 +0330

    [EDIT] product call customer

--- src/components/features/product/product-call-customer.tsx
+++ src/components/features/product/product-call-customer.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/shadcnUi/button";
 import { Phone } from "lucide-react";
 
-function ProductCallCustomer() {
+function ProductCallCustomer({ phone }: { phone: string }) {
   return (
-    <Button className="flex items-center gap-2 mt-3">
-      <Phone />
+    <Button asChild className="flex items-center gap-2 mt-3">
+      <a className="w-full" href={`tel:${phone}`}>
+        <Phone />
 
-      <span>تماس با فروشنده</span>
+        <span>تماس با فروشنده</span>
+      </a>
     </Button>
   );
 }
