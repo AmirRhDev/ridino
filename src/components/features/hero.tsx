b65b8b6bbd0daaabd3511300d2c2c085b01eb004
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-09-13 10:25:06 +0330

    [EDIT] hero section, add background

--- src/components/features/hero.tsx
+++ src/components/features/hero.tsx
@@ -1,14 +1,18 @@
 import { siteConfig } from "@/config/site";
+import { BackgroundRippleEffect } from "../shadcnUi/background-ripple-effect";
 
 function Hero() {
   return (
-    <div className="text-center py-14 mt-5">
-      <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold text-foreground">
-        {siteConfig.hero.title}
-      </h1>
-      <p className="text-foreground/70 text-lg sm:text-xl font-semibold mt-2">
-        {siteConfig.hero.description}
-      </p>
+    <div>
+      <BackgroundRippleEffect />
+      <div className="text-center py-14 mt-5 relative z-10 min-h-80">
+        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold text-foreground">
+          {siteConfig.hero.title}
+        </h1>
+        <p className="text-foreground/70 text-lg sm:text-xl font-semibold mt-2">
+          {siteConfig.hero.description}
+        </p>
+      </div>
     </div>
   );
 }
