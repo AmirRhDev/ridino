acb23b7caae53e73754a09b187f444c39f295dae
AmirRhDev <amir.rahimnezhad79@gmail.com>
2025-09-13 10:19:56 +0330

    [ADD product skeleton loader

---
+++ src/components/features/product-list/product-loader.tsx
@@ -0,0 +1,34 @@
+import { Skeleton } from "@/components/shadcnUi/skeleton";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  className?: string;
+}
+
+function ProductLoader({ className }: Props) {
+  return (
+    <div
+      className={cn(
+        "grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4",
+        className,
+      )}
+    >
+      {Array.from({ length: 12 }).map((_, i) => (
+        <div key={i} className="flex flex-col gap-2">
+          <Skeleton className="aspect-[4/3] rounded" />
+          <div className="flex items-center justify-between">
+            <Skeleton className="h-3 w-1/2" />
+            <Skeleton className="h-2 w-10" />
+          </div>
+          <Skeleton className="h-3 w-24" />
+          <div className="flex items-center justify-between">
+            <Skeleton className="h-3 w-16" />
+            <Skeleton className="h-3 w-20" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProductLoader;
